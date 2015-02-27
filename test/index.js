d423d28af299c9fbfa3832130db2b8c92877805d
vrunoa <vruno.alassia@gmail.com>
2015-02-26 23:43:00 -0300

    update test

--- test/index.js
+++ test/index.js
@@ -1,12 +1,23 @@
 import html5template from '../lib/';
 import fs from 'fs';
+import colors from 'colors';
 
-describe("Apigiphy instance test", () => {
-  it("Test default template creatin", (done) => {
-    html5template({});
-    if(fs.existsSync("./index.html")) {
-      done();
+describe("html5template tests", () => {
+
+  it("Test template creation", (done) => {
+  
+    if(fs.existsSync(__dirname + "/template.html")) {
+      fs.unlinkSync(__dirname + "/template.html");
     }
-    throw new Error("Error creating default template");
+    html5template({name:"template.html", path:__dirname});
+    if(fs.existsSync(__dirname + "/template.html")) {
+      // template created
+      fs.unlinkSync(__dirname + "/template.html");
+      done();
+    }else{
+      throw new Error("Error creating default template");
+    }
+    
   });
+
 });
